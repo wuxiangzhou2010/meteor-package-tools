946062a43e12c22fc2fb5ce7ffaf686d51749320
wuxiangzhou2010 <wuxiangzhou2010@gmail.com>
2019-03-14 11:12:01 +0800

    update mubiale:tools 0.0.1

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: 'ethereum:tools',
+  name: 'mubiale:tools',
   summary: 'Helper functions for dapps',
-  version: '0.7.0',
-  git: 'http://github.com/ethereum/meteor-package-tools'
+  version: '0.0.1',
+  git: 'http://github.com/wuxiangzhou2010/meteor-package-tools'
 });
 
 Package.onUse(function(api) {
@@ -18,7 +18,7 @@ Package.onUse(function(api) {
   //api.use('numeral:numeral@1.5.3', ['client', 'server']);
   api.use('3stack:bignumber@2.0.0', 'client');
 
-  api.use('ethereum:web3@0.15.1', ['client', 'server']);
+  api.use('mubiale:web3@0.0.2', ['client', 'server']);
   api.use('frozeman:persistent-minimongo@0.1.8', 'client');
   api.use('frozeman:storage@0.1.8', 'client');
 
@@ -33,7 +33,7 @@ Package.onUse(function(api) {
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('ethereum:tools');
+  api.use('mubiale:tools');
   api.addFiles('ethtools-tests.js', ['client', 'server']);
 });
 
